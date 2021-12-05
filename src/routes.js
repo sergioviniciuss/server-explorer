faf1e84624e6415b3017f5c6f25258c832c2f461
Sergio Lucena <sergioviniciuss@gmail.com>
2021-12-05 01:19:28 +0100

    refactor(routes): centralizes route names in one place to keep it DRY

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,21 @@
 import React from "react";
 import Login from "./pages/Login";
-// import Servers from './pages/Servers';
-// import Error from './pages/Error';
+import Servers from "./pages/Servers";
+
+export const ROUTES = {
+  LOGIN: '/login',
+  SERVERS: '/servers'
+}
 
 export const routesConfig = [
   {
     id: "1",
-    url: "/login",
+    url: ROUTES.LOGIN,
     component: Login,
   },
-  // {
-  //   id: '2',
-  //   url: '/servers',
-  //   component: Servers,
-  // },
-  // {
-  //   id: '3',
-  //   url: '/error',
-  //   component: Error,
-  // },
+  {
+    id: "2",
+    url: ROUTES.SERVERS,
+    component: Servers,
+  },
 ];
