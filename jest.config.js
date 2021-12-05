86c964d7141228fa182f224ea65cfec020597cf4
Sergio Lucena <sergioviniciuss@gmail.com>
2021-12-05 23:12:59 +0100

    fix(jest setup): Mocks correctly when components are loading files (eg: svgs)

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
   setupFilesAfterEnv: ["<rootDir>/.jest/setup.js"],
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/__mocks__/fileMock.js",
+      "<rootDir>/.jest/fileMock.js",
     "\\.(scss|less)$": "identity-obj-proxy",
   },
 };
