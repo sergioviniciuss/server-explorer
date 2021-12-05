70bceda15c8c3ae557ce4b74a252b4976f8275e8
Sergio Lucena <sergioviniciuss@gmail.com>
2021-12-05 01:29:50 +0100

    feat(api): introduces request to servers list

--- src/api/index.js
+++ src/api/index.js
@@ -13,3 +13,16 @@ export const getAuthToken = async ({ username, password }) => {
     throw new Error(errorMessage || `Unexpected error.\n${error}`);
   }
 };
+
+export const getServersList = async ({ token }) => {
+  try {
+    const response = await axios.get(ENDPOINTS.servers, {
+      headers: { Authorization: token },
+    });
+
+    return response?.data;
+  } catch (error) {
+    const errorMessage = error?.response?.data?.error_message;
+    throw new Error(errorMessage || `Unexpected error.\n${error}`);
+  }
+};
