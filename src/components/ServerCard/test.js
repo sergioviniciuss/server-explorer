f03d81fea52a8b10208f053cde33f000c9634fcd
Sergio Lucena <sergioviniciuss@gmail.com>
2021-12-05 23:16:52 +0100

    test(ServerCard): Adds coverage for the component

---
+++ src/components/ServerCard/test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { ServerCard } from ".";
+
+describe("ServerCard", () => {
+  it("renders ServerCard component with name and distance", () => {
+    const { container } = render(<ServerCard name="Germany #01" distance={123} />);
+
+    expect(screen.getByText(/Germany #01/i)).toBeInTheDocument();
+    expect(screen.getByText(/123/i)).toBeInTheDocument();
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
