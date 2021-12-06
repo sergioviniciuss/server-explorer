8f291e30a3bfe12e8b0a3610954ba8472c7836d1
Sergio Lucena <sergioviniciuss@gmail.com>
2021-12-06 02:22:27 +0100

    feat(AuthenticatedHeader): Allows user to Logout

--- src/components/AuthenticatedHeader/index.js
+++ src/components/AuthenticatedHeader/index.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { Link } from "../Link";
 import nordVPNLogo from "../../images/nordaccount-dark.svg";
 import styles from "./styles.scss";
 
+export const AuthenticatedHeader = () => {
+  const handleLogout = () => window.location.reload();
 
-export const AuthenticatedHeader = () => (
-  <div className={styles.headerContainer}>
-    <img className={styles.logo} src={nordVPNLogo} />
-    <span className={styles.greetings}>Hello there!</span>
-  </div>
-);
+  return (
+    <div className={styles.headerContainer}>
+      <img className={styles.logo} src={nordVPNLogo} />
+      <span className={styles.greetings}>
+        Hello there! <Link text="(logout)" onClick={handleLogout} />
+      </span>
+    </div>
+  );
+};
